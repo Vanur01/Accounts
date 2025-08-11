11ec27286ce002a11ef9c8a2a938b2a252da1a72
--global <patisantosh00@gmail.com>
2025-08-11 16:05:20 +0530

    change hero page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,6 @@ import Navigation from "../../components/Navigation";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Navigation />
       <main>{children}</main>
     </>
   );
